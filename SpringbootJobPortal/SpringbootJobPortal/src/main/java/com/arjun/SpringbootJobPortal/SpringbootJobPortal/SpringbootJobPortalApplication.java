7a6837d7f5fe641187aa3de3aba4eec6d73222ac
ShadowSlayer03 <sslayer441@gmail.com>
2025-07-06 16:13:57 +0530

    Added Job portal project made with SpringBoot

---
+++ SpringbootJobPortal/SpringbootJobPortal/src/main/java/com/arjun/SpringbootJobPortal/SpringbootJobPortal/SpringbootJobPortalApplication.java
@@ -0,0 +1,12 @@
+package com.arjun.SpringbootJobPortal.SpringbootJobPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+`1public class SpringbootJobPortalApplication {11
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJobPortalApplication.class, args);
+	}
+}
